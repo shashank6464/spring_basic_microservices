53dd90719813a3476cdd53aa473cac35f9775a4f
1034517 <shashank.kumar@blueyonder.com>
2022-11-02 09:21:32 +0530

    first

---
+++ src/main/java/com/base/microservices/baseMicroservices/BaseMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.base.microservices.baseMicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/base/microservices/baseMicroservices/BaseMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.base.microservices.baseMicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
